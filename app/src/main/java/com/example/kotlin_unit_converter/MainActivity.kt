b63c86f517661e9d95502d5932cd5ad3c0cd1afe
TramLeManh <ITITIU21077@student.hcmiu.edu.vn>
2024-08-24 15:10:08 +0700

    First Commit

--- app/src/main/java/com/example/kotlin_unit_converter/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_unit_converter/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Kotlin_Unit_ConverterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
