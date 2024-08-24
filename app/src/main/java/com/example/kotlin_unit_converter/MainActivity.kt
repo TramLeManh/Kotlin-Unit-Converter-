6112561a29c56253a2f813cf7e39a8ac33062d9b
TramLeManh <ITITIU21077@student.hcmiu.edu.vn>
2024-08-24 23:35:34 +0700

    Day 2

--- app/src/main/java/com/example/kotlin_unit_converter/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_unit_converter/MainActivity.kt
@@ -1,29 +1,46 @@
 package com.example.kotlin_unit_converter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlin_unit_converter.ui.theme.Kotlin_Unit_ConverterTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Kotlin_Unit_ConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Test",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Widget()
                 }
             }
         }
@@ -31,17 +48,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        color = Color.Cyan,
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Kotlin_Unit_ConverterTheme {
-        Greeting("Android")
+fun Widget() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Unit Converter")
+        Spacer(modifier = Modifier.padding(8.dp))
+        OutlinedTextField(value = "input", onValueChange = {
+            //What should happen, when the value changes
+        }, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            val context = LocalContext.current
+            Box {
+                Button(onClick = {
+                    Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text("Select")
+                    Icon(Icons.Filled.ArrowDropDown, contentDescription = "Localized description")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { "Test" }, onClick = { /*TODO*/ })
+                }
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Box() {
+                Button(onClick = {
+                    Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text("Click me")
+                    Icon(Icons.Filled.ArrowDropDown, contentDescription = "Localized description")
+                }
+
+            }
+
+        }
+
     }
 }
+
+//Use for debug widget
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Widget();
+}
+
